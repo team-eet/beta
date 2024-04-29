c84a423583f7a0ff380a48397501aba66abc556e
Dhruv Patel <peafowlemp@free.stulive.com>
2024-04-29 15:15:43 +0530

    EETNEW

---
+++ components/Common/BreadCrumb.js
@@ -0,0 +1,81 @@
+import Link from "next/link";
+import {useEffect, useState} from "react";
+import Axios from "axios";
+import API_URL from "@/pages/constant";
+import {useRouter} from "next/router";
+import {ErrorDefaultAlert} from "@/components/services/SweetAlert";
+const BreadCrumb = ({ title, text }) => {
+  const router = useRouter()
+  const REACT_APP = API_URL
+
+  const [regId, setregId] = useState('')
+  const [showContinue, setshowContinue] = useState(false)
+  useEffect(() => {
+    if(localStorage.getItem('userData')) {
+      setregId(JSON.parse(localStorage.getItem('userData')).regid)
+    }
+    Axios.get(`${REACT_APP.API_URL}/api/TutorBasics/GetTutorProfile/${JSON.parse(localStorage.getItem('userData')).regid}`, {
+      headers: {
+        ApiKey: `${REACT_APP.API_KEY}`
+      }
+    })
+        .then(res => {
+         // console.log(res.data)
+          // if(res.data.)
+          if(res.data[0].cnt !== 0) {
+            setshowContinue(true)
+          } else {
+            setshowContinue(false)
+          }
+        })
+        .catch(err => {
+          { ErrorDefaultAlert(err) }
+        })
+  }, [])
+  return (
+
+      // linear-gradient(270deg, var(--color-secondary) 0, var(--color-primary) 100%) !important
+    <div className="rbt-breadcrumb-default ptb--100 ptb_md--50 ptb_sm--30 bg-gradient-9">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="breadcrumb-inner text-center">
+              {/*<div className={'bg-gradient-10 text-dark'}>abc</div>*/}
+              <h2 className="title">{title}</h2>
+              <ul className="page-list">
+                <li className="rbt-breadcrumb-item">
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <div className="icon-right">
+                    <i className="feather-chevron-right"></i>
+                  </div>
+                </li>
+                <li className="rbt-breadcrumb-item active">{text}</li>
+              </ul>
+              {showContinue ? <>
+                <Link
+                    className="rbt-btn btn-gradient hover-icon-reverse radius-round mt-3 ms-3"
+                    href="/become-a-tutor/basics"
+                >
+                    <span className="icon-reverse-wrapper">
+                      <span className="btn-text">Continue</span>
+                      <span className="btn-icon">
+                        <i className="feather-arrow-right"></i>
+                      </span>
+                      <span className="btn-icon">
+                        <i className="feather-arrow-right"></i>
+                      </span>
+                    </span>
+                </Link>
+              </> : <></>}
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BreadCrumb;
